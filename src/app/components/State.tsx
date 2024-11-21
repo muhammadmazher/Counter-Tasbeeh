50917401cdefb09f6fd5119450ff87f6ff042ac9
Muhammad Mazher Khan <muhammadmazher@gmail.com>
2024-11-21 19:02:30 +0500

    border shaddow added

--- src/app/components/State.tsx
+++ src/app/components/State.tsx
@@ -5,11 +5,11 @@ import {useState} from "react"
 function State(){
     const [count , setCount] = useState(0)
     return(
-        <div className="border sm:rounded-lg  w-full h-full py-20  bg-emerald-300 text-center">
-            <div className="border-2 border-yellow-200 mx-10 py-10">
+        <div className="border sm:rounded-lg  w-full h-full py-10 px-10  bg-emerald-300 text-center">
+            <div className="border-2 border-yellow-200 bg-transparent shadow-lg rounded-2xl py-10">
             <div>
                 <p className="sm:text-center text-5xl pt-4"> {count} </p>
-                <button onClick={() => setCount(count + 1) } className="bg-slate-400 shadow-md border border-slate-500 py-6 px-6 rounded-full mx-4 mt-40 hover:bg-slate-500 ">  </button><br/>
+                <button onClick={() => setCount(count + 1) } className="bg-slate-400 shadow-lg border border-slate-500 py-6 px-6 rounded-full mx-4 mt-40 ">  </button><br/>
                 <p className="text-gray-800 text-sm">Count</p>
             </div>
 
