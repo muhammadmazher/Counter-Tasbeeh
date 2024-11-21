08fc4101d715053f1760357dc7169bbf68560404
Muhammad Mazher Khan <muhammadmazher@gmail.com>
2024-11-21 18:47:07 +0500

    border added

--- src/app/components/State.tsx
+++ src/app/components/State.tsx
@@ -5,16 +5,18 @@ import {useState} from "react"
 function State(){
     const [count , setCount] = useState(0)
     return(
-        <div className="border sm:rounded-lg  w-4/12 max-h-full py-20  bg-emerald-200 text-center">
+        <div className="border sm:rounded-lg  w-full h-full py-20  bg-emerald-300 text-center">
+            <div className="border-2 border-yellow-200 mx-10 py-10">
             <div>
                 <p className="sm:text-center text-5xl pt-4"> {count} </p>
-                <button onClick={() => setCount(count + 1) } className="bg-slate-400 shadow-md border-slate-600 py-6 px-6 rounded-full mx-4 mt-40 ">  </button><br/>
+                <button onClick={() => setCount(count + 1) } className="bg-slate-400 shadow-md border border-slate-500 py-6 px-6 rounded-full mx-4 mt-40 hover:bg-slate-500 ">  </button><br/>
                 <p className="text-gray-800 text-sm">Count</p>
             </div>
 
             <div className=" pb">
-                <button onClick={() => setCount(0)} className="bg-slate-500 rounded-full ml py-2 px-2 shadow-sm mt-32"></button>
+                <button onClick={() => setCount(0)} className="bg-slate-600 hover:bg-slate-700 rounded-full ml py-2 px-2 shadow-sm mt-32"></button>
                 <p className="text-gray-800 text-xs ">Reset</p>
+            </div>
             </div>
 
         </div>
